1f4e080d987322bae88046ade98aafafc392e3eb
Ramveer Singh <ramveersingharaon@gmail.com>
2024-05-22 21:14:04 +0530

    First Comment

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        "Primary":["Roboto", 'sans-serif'],
+        "logo":["Lobster Two"]
+      }
+    },
+
+  },
+  plugins: [],
+}
+
